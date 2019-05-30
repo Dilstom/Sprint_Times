fb2fc63847eb5a8e8d2e7179388eeab81786a56c
Dil Sombayeva <dilyasom@gmail.com>
2019-05-30 12:55:29 -0700

    toggle className depending on passed props - if the `tab` prop matches the `selectedTab` prop

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,9 +4,10 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+ //  console.log('props in Tab: ', props);
   return (
     <div
-      className={''}
+   className={props.selectedTab === props.tab ? 'tab active-tab' : 'tab'}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
