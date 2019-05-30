e6d18597af1bf2e8145803add1a6f3c4f7c7bcdb
Dil Sombayeva <dilyasom@gmail.com>
2019-05-30 12:57:42 -0700

    map over the tabs and create new Tab component for each one

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
+ /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
  //  console.log('props in Tab: ', props);
-  return (
-    <div
+ return (
+  <div
    className={props.selectedTab === props.tab ? 'tab active-tab' : 'tab'}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
+   onClick={() => {
+    /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
     props.selectTabHandler(props.tab);
-      }}
-    >
-      {props.tab.toUpperCase()}
-    </div>
-  );
+   }}
+  >
+   {props.tab.toUpperCase()}
+  </div>
+ );
 };
 
 // Make sure you include PropTypes on your props.
